0f91f86b42d91b2945edb0eb76bfa0164977887d
Katleho Myeza <katleho.myeza@bbd.co.za>
2025-03-19 15:29:41 +0200

    fixed

--- flyway/migrations/versioned/V202503190959__CreateUserConversation.sql
+++ flyway/migrations/versioned/V202503190959__CreateUserConversation.sql
@@ -1,5 +1,6 @@
 CREATE TABLE UserConversations (
   user_id INT NOT NULL REFERENCES users(user_id), 
   conversation_id INT NOT NULL REFERENCES Conversations(conversation_id),
-  PRIMARY KEY (user_id, conversation_id)
+  userconversation_id INT NOT NULL,
+  PRIMARY KEY (userconversation_id)
 );
